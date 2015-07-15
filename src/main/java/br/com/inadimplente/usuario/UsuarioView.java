2aa9b0f0ebd84a5911d6d11a0f6246087d6103df
Jonas S. Bezerra <jonassantosbezerra@gmail.com>
2015-07-15 01:41:22 -0300

    Início da criação do CRUD de usuário

---
+++ src/main/java/br/com/inadimplente/usuario/UsuarioView.java
@@ -0,0 +1,31 @@
+package br.com.inadimplente.usuario;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.inadimplente.kernel.AbstractView;
+
+@Named
+@ViewScoped
+public class UsuarioView extends AbstractView<Usuario> {
+
+	public UsuarioView() {
+		super(Usuario.class);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private UsuarioDAO usuarioDAO;
+	
+	@SuppressWarnings("unchecked")
+	public UsuarioDAO getDao() {
+		return usuarioDAO;
+	}
+	
+	public TipoUsuario[] getTiposDeUsuarios() {
+		return TipoUsuario.values();
+	}
+
+}
