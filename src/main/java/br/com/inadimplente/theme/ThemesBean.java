01592b6fb7c7805023f3ccbfdb281c80f230e525
Jonas S. Bezerra <jonassantosbezerra@gmail.com>
2015-07-16 14:41:43 -0300

    Novo tema

--- src/main/java/br/com/inadimplente/theme/ThemesBean.java
+++ src/main/java/br/com/inadimplente/theme/ThemesBean.java
@@ -10,7 +10,7 @@ import javax.inject.Named;
 public class ThemesBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
-	private String theme = "cupertino";
+	private String theme = "sunny";
 	
 	public String getTheme(){
 		return theme;
