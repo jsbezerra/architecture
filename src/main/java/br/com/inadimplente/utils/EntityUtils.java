d23d81582de8e98246c621def4975aa4def4b48e
Jonas S. Bezerra <jonassantosbezerra@gmail.com>
2015-07-16 15:23:01 -0300

    TODO

--- src/main/java/br/com/inadimplente/utils/EntityUtils.java
+++ src/main/java/br/com/inadimplente/utils/EntityUtils.java
@@ -7,7 +7,8 @@ import javax.persistence.Id;
 public class EntityUtils {
 	
 	private EntityUtils() {}
-	
+
+	// TODO esse método só funciona se a anotação @Id estiver no atributo e não no método. Verificar se há uma forma melhor de fazer isso.
 	@SuppressWarnings("rawtypes")
 	public static Object getId(Object entity, Class entityClass) {
 		Field[] fields = entityClass.getDeclaredFields();
