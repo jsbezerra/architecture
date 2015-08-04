6911cab95e69337366a40d2baa72a03f086c9e2d
Jonas S. Bezerra <jonassantosbezerra@gmail.com>
2015-08-04 12:31:48 -0300

    Definindo o entityManager a ser utilizado pelo picketlink

--- src/main/java/br/com/inadimplente/kernel/EntityManagerProducer.java
+++ src/main/java/br/com/inadimplente/kernel/EntityManagerProducer.java
@@ -1,21 +1,18 @@
 package br.com.inadimplente.kernel;
 
-import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.picketlink.annotations.PicketLink;
+
 public class EntityManagerProducer {
 	
 	@PersistenceContext(unitName="inadimplentes")
-	private EntityManager entityManager;
-	
 	@Produces
 	@Default
-	@RequestScoped
-	public EntityManager getEntityManager(){
-		return entityManager;
-	}
-
+	@PicketLink
+	private EntityManager entityManager;
+	
 }
