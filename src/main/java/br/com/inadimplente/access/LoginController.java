96ad8ebfb2dbda526d753d8788d19db933e660b0
Jonas S. Bezerra <jonassantosbezerra@gmail.com>
2015-08-05 10:02:39 -0300

    Início do processo de autenticação com Shiro

--- src/main/java/br/com/inadimplente/access/LoginController.java
+++ src/main/java/br/com/inadimplente/access/LoginController.java
@@ -1,11 +1,11 @@
 package br.com.inadimplente.access;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.apache.shiro.SecurityUtils;
@@ -13,17 +13,12 @@ import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 
-import br.com.inadimplente.kernel.MessagesHandler;
-
 @Named
 @RequestScoped
 public class LoginController {
 
 	private static final Logger LOG = Logger.getLogger(LoginController.class.getName());
 	
-	@Inject
-	private MessagesHandler messagesHandler;
-
 	private String username;
 	private String password;
 	boolean remember = false;
@@ -35,10 +30,10 @@ public class LoginController {
 		try {
 			currentUser.login(token);
 		} catch (AuthenticationException e) {
-			LOG.warning(e.getMessage());
-			messagesHandler.error("#{messages['login.failed']}");
-			/*FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage("Login Failed: " + e.getMessage(), e.toString()));*/
+			LOG.log(Level.SEVERE, e.getMessage());
+			//TODO consertar isso aqui
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuário ou Senha inválidos", ""));
 			return "/index";
 		}
 		return "/home.xhtml?faces-redirect=true";
