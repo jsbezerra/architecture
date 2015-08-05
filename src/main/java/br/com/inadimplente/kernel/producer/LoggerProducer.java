f858f9fa515cd1f46dbbec39736881c2e01a5644
Jonas S. Bezerra <jonassantosbezerra@gmail.com>
2015-08-05 13:42:24 -0300

    Adicionado log do slf4j ao projeto

---
+++ src/main/java/br/com/inadimplente/kernel/producer/LoggerProducer.java
@@ -0,0 +1,15 @@
+package br.com.inadimplente.kernel.producer;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerProducer {
+    
+    @Produces
+    public Logger produceLogger(InjectionPoint injectionPoint) {
+        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().toString());
+    }
+}
