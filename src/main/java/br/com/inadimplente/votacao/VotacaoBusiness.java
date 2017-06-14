23da5f3d7961d00661738c24eacbec8de878a948
Jonas S. Bezerra <jsbezerra@inf.ufrgs.br>
2017-06-14 20:06:19 -0300

    Updating VotacaoBusiness

--- src/main/java/br/com/inadimplente/votacao/VotacaoBusiness.java
+++ src/main/java/br/com/inadimplente/votacao/VotacaoBusiness.java
@@ -2,10 +2,13 @@ package br.com.inadimplente.votacao;
 
 import br.com.inadimplente.restaurante.Restaurante;
 import br.com.inadimplente.restaurante.RestauranteDAO;
+import br.com.inadimplente.usuario.UsuarioDAO;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
+import java.util.Date;
 import java.util.List;
 
 @Named
@@ -19,6 +22,12 @@ public class VotacaoBusiness {
     @Inject
     private RestauranteDAO restauranteDAO;
 
+    @Inject
+    private UsuarioDAO usuarioDAO;
+
+    @Inject
+    private VotoDAO votoDAO;
+
     public Restaurante getRestaurante() {
         return restaurante;
     }
@@ -27,20 +36,14 @@ public class VotacaoBusiness {
         this.restaurante = restaurante;
     }
 
-    public VotacaoDAO getVotacaoDAO() {
-        return votacaoDAO;
-    }
-
-    public void setVotacaoDAO(VotacaoDAO votacaoDAO) {
-        this.votacaoDAO = votacaoDAO;
-    }
-
-    public RestauranteDAO getRestauranteDAO() {
-        return restauranteDAO;
-    }
-
-    public void setRestauranteDAO(RestauranteDAO restauranteDAO) {
-        this.restauranteDAO = restauranteDAO;
+    @PostConstruct
+    @Transactional
+    public void prepararVotacao() {
+        if (!votacaoDAO.hasVotacaoAberta()) {
+            Votacao votacao = new Votacao();
+            votacao.setData(new Date());
+            votacaoDAO.create(votacao);
+        }
     }
 
     @Transactional
@@ -49,7 +52,22 @@ public class VotacaoBusiness {
 
     }
 
+    /*TODO:
+        - ocultar botão quando não houver nenhum restaurante
+        - não trazer restaurantes já vencedores nessa semana na pesquisa */
     public List<Restaurante> restaurantesDisponiveis() {
         return restauranteDAO.listAll();
+    }
+
+    public void votar() {
+        Voto voto = new Voto();
+        voto.setRestaurante(getRestaurante());
+        voto.setUsuario(usuarioDAO.findByLogin("jonas"));
+        voto.setVotacao(votacaoDAO.findVotacaoAtual());
+        votoDAO.create(voto);
+    }
+
+    public boolean canVote() {
+        return restauranteDAO.count() > 0;
     }
 }
