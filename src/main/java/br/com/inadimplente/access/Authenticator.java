4cf7b38b96fe6c5a72724e15813d7c464962d3e1
Jonas S. Bezerra <jsbezerra@inf.ufrgs.br>
2017-06-14 19:06:04 -0300

    Created authenticator to get logged user information

---
+++ src/main/java/br/com/inadimplente/access/Authenticator.java
@@ -0,0 +1,25 @@
+package br.com.inadimplente.access;
+
+import br.com.inadimplente.usuario.Usuario;
+import br.com.inadimplente.usuario.UsuarioDAO;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Authenticator {
+
+    @Inject
+    private UsuarioDAO usuarioDAO;
+
+    public Usuario getUsuarioLogado() {
+        Subject currentUser = SecurityUtils.getSubject();
+        String login = currentUser.getPrincipal().toString();
+        return usuarioDAO.findByLogin(login);
+    }
+
+}
