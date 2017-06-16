22282d37dd2cd6bb73bb6601fb54a3d6e1ddae10
Jonas S. Bezerra <jonassantosbezerra@gmail.com>
2017-06-16 02:36:32 -0300

    criada query para encontrar o voto atual quando houver

--- src/main/java/br/com/inadimplente/votacao/VotoDAO.java
+++ src/main/java/br/com/inadimplente/votacao/VotoDAO.java
@@ -1,9 +1,11 @@
 package br.com.inadimplente.votacao;
 
 import br.com.inadimplente.kernel.AbstractDAO;
+import br.com.inadimplente.usuario.Usuario;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.util.List;
 
 @Named
 @RequestScoped
@@ -11,5 +13,18 @@ public class VotoDAO extends AbstractDAO<Voto> {
 
     public VotoDAO(){
         super(Voto.class);
+    }
+
+    public Voto findByUsusarioAndVotacao(Usuario usuario, Votacao votacao) {
+        List<Voto> votos = getEntityManager()
+                .createNamedQuery("votoAtualDoUsuario", Voto.class)
+                .setParameter("usuario", usuario)
+                .setParameter("votacao", votacao)
+                .getResultList();
+        if (votos.size() > 0) {
+            return votos.get(0);
+        } else {
+            return null;
+        }
     }
 }
